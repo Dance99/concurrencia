0326749dbcbbb60e45272a270ed9ccb45a3f7df4
Daniela <nam991019@gmail.com>
2025-09-08 15:06:27 -0500

    commit1

--- main.go
+++ main.go
@@ -13,4 +13,18 @@ func main() {
 
 	//se declara una variable para contar
 	mensaje := "Este es el taller 0"
+
+	//llamamos la funcion y definimos mas abajo la funcion
+	imprimirMensaje(mensaje)
+
+	fmt.Println("Contando hasta 3:")
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("Numero %d\n", i) //permite el formateo con %d para numeros
+	}
 }
+
+//definimos la nueva función impmensaje
+//toma el argumento mensaje de tipo string
+func imprimirMensaje(mensaje string) {
+	fmt.Println("Mensaje de la función:", texto)
+ } 
