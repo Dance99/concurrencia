2b2ac841cd85a89dd0a824de4c4c9f0b4b632bb2
Daniela <nam991019@gmail.com>
2025-09-08 15:00:26 -0500

    Taller0

---
+++ go.mod
@@ -0,0 +1,3 @@
+module taller0
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,16 @@
+//main-go
+
+package main //paqute "main"
+
+//importación de paquetes
+//"fmt" es para formatear e imprimir el texto
+import "fmt"
+
+//funcion main es el punto de entrada del programa
+func main() {
+	//se usa el paquete fmt 
+	fmt.Println("Hola Mundo Concurrente")
+
+	//se declara una variable para contar
+	mensaje := "Este es el taller 0"
+}
